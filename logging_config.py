74afc3dea1f1e9747de7816cb87be4add3b37546
Mohamed Jaouahar <123220941+Lordozer@users.noreply.github.com>
2024-05-31 07:11:34 -0400

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -1,9 +1,10 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 def setup_logging():
+    handler = RotatingFileHandler('scanner.log', maxBytes=1000000, backupCount=3)
     logging.basicConfig(
-        filename='scanner.log', 
-        level=logging.INFO, 
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        handlers=[handler],
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(module)s - %(message)s'
     )
-                                                                                                                                                                                                                                            
